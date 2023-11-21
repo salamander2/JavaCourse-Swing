595ded07cfae1652e00941d5ca672cc71bb1cdfd
Salamander2 <mk_gecko@yahoo.com>
2023-11-21 15:41:31 -0500

    Update FlowingColour.java

--- src/unit1/FlowingColour.java
+++ src/unit1/FlowingColour.java
@@ -1,6 +1,5 @@
 package unit1;
 
-
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,38 +10,31 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class FlowingColour implements ActionListener {
-	public static void main(String[] args) {
-		new FlowingColour();		
-	}
+	public static void main(String[] args) { new FlowingColour(); }
 
+	//Only make global what you absolutely need to.
 	JFrame window = new JFrame("Flowing Colours");
 	int winW = 350, winH = 500;
-	Timer timer;
-	
-	FlowingColour() {
 		
+	FlowingColour() {		
 		window.setSize(winW,winH);
-		//window.setLocationRelativeTo(null); // Always sets it to the middle of the screen
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel pnl1 = new JPanel();
-		pnl1.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
-		
-		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
 		
 		for (int i=0; i < 20000; i++) {
-			pnl1.add(new ColourLabel());
+			panel.add(new ColourLabel());
 		}
-	
 
-		window.add(pnl1);// Last ever thing to do
+		window.add(panel);
 		window.setVisible(true);
 		
-		timer = new Timer(5, this);
+		//Time does not need to be global. We're never interacting with it again.
+		Timer timer = new Timer(5, this);
 		timer.start();
-
 	}
 
-	class ColourLabel extends JLabel {		
+	private class ColourLabel extends JLabel {		
 		ColourLabel() {
 			this.setText(" ");			
 			this.setBackground(ColourRainbow.getNextColour());
@@ -53,9 +45,7 @@ public class FlowingColour implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		winW +=1;
-		window.setSize(winW, winH);
-		window.repaint();
-		
+		window.setSize(++winW, winH);
+		window.repaint(); //this updates the graphics		
 	}
 }
